1fa1cd772a572b63cf65ddb307125402d267679c
tana965 <2dana2015@gmail.com>
2023-06-17 08:30:57 +0300

    Add interface

---
+++ 34567/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34567
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
